53a79628a0d2002f7a54b2d32102ea728c1c6dfb
Tien Manh <happyhere1995@gmail.com>
2015-04-23 14:26:22 +0700

    test with capri

--- Broadconfig.mk
+++ Broadconfig.mk
@@ -1,40 +1,35 @@
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
--include vendor/samsung/corsica/BoardConfigVendor.mk
-
-# Legacy MMAP for pre-lollipop blobs
-BOARD_USES_LEGACY_MMAP := true
+-include vendor/samsung/i9082/BoardConfigVendor.mk
 
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := rhea
+TARGET_BOARD_PLATFORM := capri
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a9
-
-TARGET_ARCH_LOWMEM := true
 TARGET_CPU_SMP := true
 
-TARGET_BOOTLOADER_BOARD_NAME := rhea
-TARGET_NO_RADIOIMAGE := true
+TARGET_BOOTLOADER_BOARD_NAME := capri
 
-BOARD_KERNEL_CMDLINE := androidboot.console=ttyS0
-BOARD_KERNEL_BASE := 0x82000000
+BOARD_KERNEL_CMDLINE := console=ttyS0,115200n8 mem=832M@0xA2000000 androidboot.console=ttyS0 vc-cma-mem=0/176M@0xCB000000
+BOARD_KERNEL_BASE := 0xa2000000
 BOARD_KERNEL_PAGESIZE := 4096
 
 TARGET_USERIMAGES_USE_EXT4 := true
 
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00800000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00800000
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
 
-# This is actually 1002438656, but reducing to 0 MB to support users using repartition.
+# This is actually 1610612736, but reducing to 1049 MB to support users using repartition.
 # Feel free to increase when needed
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1002438656
+# See: http://forum.xda-developers.com/showpost.php?p=55293011&postcount=1
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1099956224
 
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 2173120512
-BOARD_CACHEIMAGE_PARTITION_SIZE := 573741824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 4404019200
+BOARD_CACHEIMAGE_PARTITION_SIZE := 1073741824
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072
 
@@ -42,16 +37,16 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 EXTENDED_FONT_FOOTPRINT := true
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/corsica
-TARGET_KERNEL_CONFIG := cyanogenmod_corsica_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_i9082_defconfig
+
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/corsica/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := device/samsung/corsica/libbt_vndcfg.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9082/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/i9082/libbt_vndcfg.txt
 
 # Connectivity - Wi-Fi
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
@@ -60,7 +55,7 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_HOSTAPD_DRIVER        := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
 BOARD_WLAN_DEVICE           := bcmdhd
-BOARD_WLAN_DEVICE_REV       := bcm4330
+BOARD_WLAN_DEVICE_REV       := bcm4334
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
@@ -73,19 +68,16 @@ WIFI_BAND                   := 802_11_ABG
 # Wi-Fi Tethering
 BOARD_HAVE_SAMSUNG_WIFI := true
 
+# SkTextBox for libtvout
+BOARD_USES_SKTEXTBOX := true
+
 # Hardware rendering
 USE_OPENGL_RENDERER := true
-BOARD_EGL_CFG := device/samsung/corsica/egl.cfg
+BOARD_EGL_CFG := device/samsung/i9082/egl.cfg
 BOARD_USE_MHEAP_SCREENSHOT := true
 BOARD_EGL_WORKAROUND_BUG_10194508 := true
 TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS -DRHEA_HWC -DREFBASE_JB_MR1_COMPAT_SYMBOLS
-COMMON_GLOBAL_CFLAGS += -DEGL_NEEDS_FNW -DSTE_HARDWARE -DICS_AUDIO_BLOB -DHAVE_PRE_KITKAT_AUDIO_POLICY_BLOB
-COMMON_GLOBAL_CFLAGS += -DSAMSUNG_BCM_AUDIO_BLOB -DMR0_CAMERA_BLOB -DMR0_AUDIO_BLOB
-BOARD_USES_STE_HARDWARE := true
-TARGET_DONT_SET_AUDIO_AAC_FORMAT := true
-BOARD_HAVE_PRE_KITKAT_AUDIO_BLOB := true
-BOARD_HAVE_PRE_KITKAT_AUDIO_POLICY_BLOB := true
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS -DCAPRI_HWC -DREFBASE_JB_MR1_COMPAT_SYMBOLS
 
 # Bootanimation
 TARGET_BOOTANIMATION_PRELOAD := true
@@ -96,26 +88,21 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 
 # RIL
-BOARD_RIL_CLASS := ../../../device/samsung/corsica/ril/
+BOARD_RIL_CLASS := ../../../device/samsung/i9082/ril/
 
 # Recovery
-TARGET_RECOVERY_FSTAB := device/samsung/corsica/fstab.rhea_ss_corsica
-BOARD_LDPI_RECOVERY := true
-BOARD_USE_CUSTOM_RECOVERY_FONT := "<font_7x16.h>"
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/corsica/recovery/recovery_keys.c
-TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
-
-# UMS
+TARGET_RECOVERY_FSTAB := device/samsung/i9082/fstab.capri_ss_baffin
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
+TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
 
 # healthd
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.rhea
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.capri
 
 # CMHW
-BOARD_HARDWARE_CLASS := device/samsung/corsica/cmhw/
+BOARD_HARDWARE_CLASS := hardware/samsung/cmhw/ device/samsung/i9082/cmhw/
 
 # GPS
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/corsica/include
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/i9082/include
 
 # Compat
 TARGET_USES_LOGD := false
@@ -125,12 +112,15 @@ MALLOC_IMPL := dlmalloc
 
 # SELinux
 BOARD_SEPOLICY_DIRS += \
-    device/samsung/corsica/sepolicy
+    device/samsung/i9082/sepolicy
 
 BOARD_SEPOLICY_UNION += \
     file_contexts \
     property_contexts \
+    service_contexts \
+    bkmgrd.te \
     device.te \
+    geomagneticd.te \
     gpsd.te \
     init.te \
     immvibed.te \
@@ -138,5 +128,6 @@ BOARD_SEPOLICY_UNION += \
     macloader.te \
     rild.te \
     shell.te \
-    netd.te \
-    surfaceflinger.te \
+    system_server.te \
+    tvserver.te \
+    vclmk.te \
