14381e26bea569cfcefdb00265d988566a2c0c7e
Tien Manh <happyhere1995@gmail.com>
2015-04-23 14:22:37 +0700

    Update Broadconfig.mk

--- Broadconfig.mk
+++ Broadconfig.mk
@@ -15,8 +15,10 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a9
 
 TARGET_ARCH_LOWMEM := true
+TARGET_CPU_SMP := true
 
 TARGET_BOOTLOADER_BOARD_NAME := rhea
+TARGET_NO_RADIOIMAGE := true
 
 BOARD_KERNEL_CMDLINE := androidboot.console=ttyS0
 BOARD_KERNEL_BASE := 0x82000000
@@ -24,16 +26,24 @@ BOARD_KERNEL_PAGESIZE := 4096
 
 TARGET_USERIMAGES_USE_EXT4 := true
 
-# fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00800000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00800000
+
+# This is actually 1002438656, but reducing to 0 MB to support users using repartition.
+# Feel free to increase when needed
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1002438656
+
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2173120512
+BOARD_CACHEIMAGE_PARTITION_SIZE := 573741824
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072
 
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/corsica
 TARGET_KERNEL_CONFIG := cyanogenmod_corsica_defconfig
-
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 
@@ -54,7 +64,6 @@ BOARD_WLAN_DEVICE_REV       := bcm4330
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
-WIFI_DRIVER_FW_PATH_P2P     := "/system/etc/wifi/bcmdhd_p2p.bin_b2"
 WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/dhd.ko"
 WIFI_DRIVER_MODULE_NAME     := "dhd"
 WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
@@ -70,12 +79,17 @@ BOARD_EGL_CFG := device/samsung/corsica/egl.cfg
 BOARD_USE_MHEAP_SCREENSHOT := true
 BOARD_EGL_WORKAROUND_BUG_10194508 := true
 TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
-BOARD_EGL_NEEDS_FNW := true
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
-COMMON_GLOBAL_CFLAGS += -DMR0_CAMERA_BLOB -DEGL_NEEDS_FNW
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS -DRHEA_HWC -DREFBASE_JB_MR1_COMPAT_SYMBOLS
+COMMON_GLOBAL_CFLAGS += -DEGL_NEEDS_FNW -DSTE_HARDWARE -DICS_AUDIO_BLOB -DHAVE_PRE_KITKAT_AUDIO_POLICY_BLOB
+COMMON_GLOBAL_CFLAGS += -DSAMSUNG_BCM_AUDIO_BLOB -DMR0_CAMERA_BLOB -DMR0_AUDIO_BLOB
+BOARD_USES_STE_HARDWARE := true
+TARGET_DONT_SET_AUDIO_AAC_FORMAT := true
+BOARD_HAVE_PRE_KITKAT_AUDIO_BLOB := true
+BOARD_HAVE_PRE_KITKAT_AUDIO_POLICY_BLOB := true
 
-# Audio
-COMMON_GLOBAL_CFLAGS += -DMR0_AUDIO_BLOB
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
@@ -94,8 +108,20 @@ TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 # UMS
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
 
+# healthd
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.rhea
+
 # CMHW
-BOARD_HARDWARE_CLASS := hardware/samsung/cmhw/ device/samsung/corsica/cmhw/
+BOARD_HARDWARE_CLASS := device/samsung/corsica/cmhw/
+
+# GPS
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/corsica/include
+
+# Compat
+TARGET_USES_LOGD := false
+
+# jemalloc causes a lot of random crash on free()
+MALLOC_IMPL := dlmalloc
 
 # SELinux
 BOARD_SEPOLICY_DIRS += \
@@ -103,23 +129,14 @@ BOARD_SEPOLICY_DIRS += \
 
 BOARD_SEPOLICY_UNION += \
     file_contexts \
-    surfaceflinger.te \
+    property_contexts \
+    device.te \
+    gpsd.te \
     init.te \
+    immvibed.te \
+    kernel.te \
+    macloader.te \
+    rild.te \
     shell.te \
     netd.te \
-    device.te \
-    rild.te \
-
-#twrp
-#DEVICE_RESOLUTION := 240x320
-#RECOVERY_GRAPHICS_USE_LINELENGTH := true
-#RECOVERY_SDCARD_ON_DATA := true
-#BOARD_HAS_NO_REAL_SDCARD := true
-#TW_INTERNAL_STORAGE_PATH := "/data/media"
-#TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-#TW_EXTERNAL_STORAGE_PATH := "/sdcard"
-#TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard"
-#TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
-#TW_MAX_BRIGHTNESS := 255
-#TW_INCLUDE_INJECTTWRP := true
-#TWRP_EVENT_LOGGING := false
+    surfaceflinger.te \
